4153b71c8d5e50ffece0830cb1d849f80343bd63
koec <fahad.aijaz4@gmail.com>
2021-07-09 14:49:29 +0500

    added imports

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from hello import Hello
 # some comment
 app = FastAPI()
 @app.get("/")
 async def hello():
-    return {'message': 'hello'}
+    hello = Hello(some_str = 'good')
+    return {'message': hello.some_str}
